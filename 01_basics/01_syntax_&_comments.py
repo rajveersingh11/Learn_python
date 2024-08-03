ea3c9ac99cc9ab7235105bece206fa98f850d893
Rajveer Singh Shekhawat <119071224+rajveersingh11@users.noreply.github.com>
2024-08-03 07:59:42 +0000

    comment and syntax

---
+++ 01_basics/01_syntax_&_comments.py
@@ -0,0 +1,30 @@
+# Syntax
+
+if 5 > 2:
+    print("five is greater than two!")
+
+# python will give error if you skip the indentation;
+#if 5 > 2:
+#print("five is greater than two!")  it will give error
+
+if 5 > 2:
+    print("five is greater than two!")
+if 4 > 2:
+    print("four is greater than two!") 
+
+"""
+we have to use the same number of spaces in the same block of code,
+otherwise python will give as an error:
+"""
+"""if 5 > 2:
+    print("five is greater than two!")
+          print("four is greater than two!")    it will give error
+          """
+
+# Comments     comments starts with a # and """ comment """, and python will ignore it   
+# This is a comment 
+""" 
+This is also a comment 
+written in
+more than just one line
+"""
